aefe25dcf91b66fab8455b2273fd617fa27841c6
Lucas Milhão <lucasmilhao683@gmail.com>
2026-09-02 19:32:02 -0300

    Resolvendo coisas do produto

--- backend/src/model/Produto.php
+++ backend/src/model/Produto.php
@@ -7,19 +7,15 @@ class Produto {
     public ?string $imagemUrl;
     public string $nome;
     public string $descricao;
-    public string $tamanho;
     public float $preco;
-    public int $estoque;
     public bool $ativo;
 
-    public function __construct(?int $id = null, ?string $imagemUrl = null, string $nome, string $descricao, string $tamanho, float $preco, int $estoque, bool $ativo) {
+    public function __construct(?int $id = null, ?string $imagemUrl = null, string $nome, string $descricao, float $preco, bool $ativo) {
         $this->id = $id;
         $this->imagemUrl = $imagemUrl;
         $this->nome = $nome;
         $this->descricao = $descricao;
-        $this->tamanho = $tamanho;
         $this->preco = $preco;
-        $this->estoque = $estoque;
         $this->ativo = $ativo;
     }
 }
--- backend/src/service/ProdutoService.php
+++ backend/src/service/ProdutoService.php
@@ -17,31 +17,25 @@ class ProdutoService
         $this->db = $database->conectar();
     }
 
-   public function criar(?string $imagemUrl, string $nome, string $descricao, string $tamanho, float $preco, int $estoque, bool $ativo) {
+   public function criar(?string $imagemUrl, string $nome, string $descricao, float $preco, bool $ativo) {
         if (empty($nome)) {
             throw new Exception("Nome do produto é obrigatório.");
         }
         if (empty($descricao)) {
             throw new Exception("Descricao do produto é obrigatória.");
         }
-        if (empty($tamanho)) {
-            throw new Exception("Tamanho do produto é obrigatório.");
-        }
         if (empty($preco)) {
             throw new Exception("Preço do produto é obrigatório.");
         }
-        if (empty($estoque)) {
-            throw new Exception("Estoque do produto é obrigatório.");
-        }
 
-        $sql = "INSERT INTO produto (imagem_url, nome, descricao, tamanho, preco, estoque, ativo) VALUES (?, ?, ?, ?, ? , ?, ?)";
+        $sql = "INSERT INTO produto (imagem_url, nome, descricao, preco, ativo) VALUES (?, ?, ?, ?, ?)";
         $stmt = $this->db->prepare($sql);
 
-        $stmt->execute([$imagemUrl, $nome, $descricao, $tamanho, $preco, $estoque, $ativo]);
+        $stmt->execute([$imagemUrl, $nome, $descricao, $preco, $ativo]);
 
         $id = $this->db->lastInsertId();
 
-        return new Produto($id, $imagemUrl, $nome, $descricao, $tamanho, $preco, $estoque, $ativo);
+        return new Produto($id, $imagemUrl, $nome, $descricao, $preco, $ativo);
     }
 
     public function listar() : array {
