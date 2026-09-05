6c65acfe1921df4a1f8dd483f11442c50241b833
arthurwilsondev <arthur.wilson.oficial@gmail.com>
2026-09-04 21:45:37 -0300

    Para testes

--- web/components/header.php
+++ web/components/header.php
@@ -1,17 +1,3 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Código 42</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/produto.css">
-</head>
-
-
 <header class="bg-dark text-light">
     <div class="container-fluid px-5 py-3">
         <div class="row align-items-center">
